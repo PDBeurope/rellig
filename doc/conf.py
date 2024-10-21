6672a2901457a4ab58893a6849e9dcaf13655fc9
roshan <roshan@ebi.ac.uk>
2024-10-21 19:11:21 +0100

    🐛 remove the definition of deleted static file

--- doc/conf.py
+++ doc/conf.py
@@ -107,7 +107,6 @@ html_logo = "logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 html_css_files = ["css/styles.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
@@ -184,4 +183,3 @@ texinfo_documents = [
 ]
 
 # endregion
-
